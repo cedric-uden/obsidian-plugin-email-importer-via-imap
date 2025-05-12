37886b8b0b76a3f0511c558e294df494107ec6df
Cédric Uden <cedric@uden.de>
2025-05-12 21:55:14 +0200

    mark messages as read after opening

--- src/main.ts
+++ src/main.ts
@@ -50,20 +50,32 @@ class EmailInfo {
     date: Date | null;
     body: string;
     isUnread: boolean | null;
+    uid: number;
 
     constructor(
         subject: string = '',
         date: Date | null = null,
         body: string = '',
-        isUnread: boolean | null = null
+        isUnread: boolean | null = null,
+        uid: number = 0,
     ) {
         this.subject = subject;
         this.date = date;
         this.body = body;
         this.isUnread = isUnread;
+        this.uid = uid;
     }
 }
 
+function markAsRead(uid: number | number[]) {
+    imap.setFlags(uid, ['\\Seen'], (err) => {
+        if (err) {
+            console.error('Error marking message as read:', err);
+        } else {
+            console.log(`Message(s) ${uid} marked as read`);
+        }
+    });
+}
 function printAvailableMailboxes() {
     imap.getBoxes((err, boxes) => {
         if (err) {
@@ -77,7 +89,7 @@ function printAvailableMailboxes() {
 
 function openInbox(cb: (err: Error | null, box: Imap.Box) => void) {
     printAvailableMailboxes();
-    imap.openBox(config.mailbox, true, cb);
+    imap.openBox(config.mailbox, false, cb);
 }
 
 imap.once('ready', function () {
@@ -97,12 +109,15 @@ imap.once('ready', function () {
         const start = Math.max(1, total - maxToFetch + 1);
         const range = `${start}:${total}`;
 
+        let messageUids: number[] = [];
+
         var f = imap.seq.fetch(range, {
             bodies: ['HEADER.FIELDS (FROM TO SUBJECT DATE)', 'TEXT'],
             struct: true
         });
         f.on('message', function (msg: any) {
             let emailInfo = new EmailInfo();
+            let uid: number;
             msg.on('body', function (stream: any, info: any) {
                 var buffer = '';
                 stream.on('data', function (chunk: any) {
@@ -122,6 +137,10 @@ imap.once('ready', function () {
             });
             msg.once('attributes', function (attrs: any) {
                 emailInfo.isUnread = !attrs['flags'].includes('\\Seen');
+                emailInfo.uid = attrs['uid'];
+                if (emailInfo.isUnread) {
+                    messageUids.push(emailInfo.uid);
+                }
             });
             msg.once('end', function () {
                 console.log(inspect(emailInfo, false, 8));
@@ -132,6 +151,11 @@ imap.once('ready', function () {
         });
         f.once('end', function () {
             console.log('Done fetching all messages!');
+
+            if (messageUids.length > 0) {
+                markAsRead(messageUids);
+            }
+
             imap.end();
         });
     });
