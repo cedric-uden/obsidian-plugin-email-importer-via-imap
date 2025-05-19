857e7c58261a34424e2e8d0ede731c59f3ede917
Cédric Uden <cedric@uden.de>
2025-05-19 12:07:21 +0200

    remove ribbon or statusbar actions / displays

--- src/main.ts
+++ src/main.ts
@@ -27,19 +27,6 @@ export default class MyPlugin extends Plugin {
     async onload() {
         await this.loadSettings();
 
-        // This creates an icon in the left ribbon.
-        const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-            // Called when the user clicks the icon.
-            new Notice('This is a notice!');
-        });
-        // Perform additional things with the ribbon
-        ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-        // This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-        const statusBarItemEl = this.addStatusBarItem();
-        statusBarItemEl.setText('Status Bar Text');
-
-        // This adds a simple command that can be triggered anywhere
         this.addCommand({
             id: 'email-to-obsidian-note',
             name: 'Fetch email to Obsidian note',
